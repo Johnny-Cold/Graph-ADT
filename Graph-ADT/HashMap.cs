3c2a9b3f85ed67ecb0702cec3b634fa298fa6ded
OJ-Mokote <obakengmokote@gmail.com>
2018-06-01 14:37:46 +0200

    Add project files.

---
+++ Graph-ADT/HashMap.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_ADT
+{
+    public abstract class HashMap<K,V> 
+    {
+        protected int capacity; // The hash map's capacity.
+        protected int numEntries;
+        private int primeFactor;
+        private long shift, scale; // The shift and scaling factors.
+        private List<K> keySet;
+
+        /// <summary>
+        /// Class constructor.
+        /// Initialises class member variables.
+        /// </summary>
+        /// <param name="capacity"> The hash map's capacity. </param>
+        /// <param name="primeFactor"> the prime factor to use. </param>
+        public HashMap(int capacity, int primeFactor)
+        {
+            Random rand = new Random();
+
+            this.capacity = capacity;
+            this.primeFactor = primeFactor;
+            scale = rand.Next(primeFactor - 1) + 1;
+            shift = rand.Next(primeFactor);
+            createTable();
+        }
+
+        /// <summary>
+        /// Class constructor.
+        /// Creates a new hash map with a specified capacity and a default prime factor.
+        /// </summary>
+        /// <param name="capacity"> The hash map's capacity. </param>
+        public HashMap(int capacity) : this(capacity, 109345121){}
+
+        /// <summary>
+        /// No-args constructor.
+        /// Creates a new hash map with default capacity and prime factor.
+        /// </summary>
+        public HashMap() : this(17) {}
+        
+        /// <summary>
+        /// Generates a hash value from the given key.
+        /// </summary>
+        /// <returns> The hash value of a given key. </returns>
+        protected int hashCode(K key)
+        {
+            return (int)((Math.Abs(key.GetHashCode() * scale + shift) % primeFactor) % capacity);
+        }
+
+        public abstract V get(K key);
+
+        public abstract V put(K key, V value);
+
+        public abstract V remove(K key);
+        
+        public List<KeyValuePair<K,V>> entrySet()
+        {
+            List<KeyValuePair<K, V>> entries = new List<KeyValuePair<K, V>>();
+
+            foreach(K key in keySet)
+            {
+                V value = get(key);
+                KeyValuePair<K, V> entry = new KeyValuePair<K, V>(key, value);
+                entries.Add(entry);
+            }
+
+            return entries;
+        }
+
+        public void resize(int newCapacity)
+        {
+            List<KeyValuePair<K, V>> buffer = new List<KeyValuePair<K, V>>(numEntries);
+            List<KeyValuePair<K,V>> entries = entrySet();
+            
+            foreach (KeyValuePair<K,V> entry in entries)
+            {
+                buffer.Add(entry);
+            }
+
+            // Starting to build the hash map from scratch using the new capacity.
+            capacity = newCapacity;
+            createTable();
+            numEntries = 0;
+            
+            foreach(KeyValuePair<K,V> entry in buffer)
+            {
+                put(entry.Key, entry.Value);
+            }
+        }
+
+        public bool contains(K key)
+        {
+            return get(key) != null;
+        }
+
+        /// <summary>
+        /// Clears the hash map.
+        /// </summary>
+        public virtual void clear()
+        {
+            foreach(K key in keySet)
+            {
+                remove(key);
+            }
+        }
+        
+        protected abstract void createTable();
+    }
+}
---
+++ Graph-ADT/SeparateChainingHashMap.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Graph_ADT
+{
+    /// <summary>
+    /// Implementation of a hash map that implements separate chaining.
+    /// </summary>
+    public class SeparateChainingHashMap<K, V> : HashMap<K, V>
+    {
+        private List<KeyValuePair<K, V>>[] table;
+        private float loadFactor = 10.0f;S
+        private int minimumSize = 1024;
+
+        public SeparateChainingHashMap() : base()
+        {
+            initialiseMap(minimumSize);
+        }
+
+        public SeparateChainingHashMap(int capacity) : base(capacity)
+        {
+            initialiseMap(capacity);
+        }
+
+        public SeparateChainingHashMap(int capacity, int primeFactor) : base(capacity, primeFactor)
+        {
+            initialiseMap(capacity);
+        }
+
+        protected override void createTable()
+        {
+            table = new List<KeyValuePair<K,V>>[capacity];
+        }
+
+        private void initialiseMap(int length)
+        {
+            table = new List<KeyValuePair<K, V>>[length];
+
+            for(int k = 0; k < table.Length; k++)
+            {
+                table[k] = new List<KeyValuePair<K, V>>(10);
+            }
+                
+            numEntries = 0;
+        }
+
+        public override V get(K key)
+        {
+            V value = default(V);
+            int hash = hashCode(key);
+            List<KeyValuePair<K, V>> bucket = table[hash];
+         
+            foreach(KeyValuePair<K,V> entry in bucket)
+            {
+                if(entry.Key.Equals(key))
+                {
+                    value = entry.Value;
+                }
+            }
+
+            return value;
+        }
+
+        public override V put(K key, V value)
+        {
+            return put(new KeyValuePair<K, V>(key, value));
+        }
+
+        private V put(KeyValuePair<K, V> pair)
+        {
+            int hash = hashCode(pair.Key);
+            bool alreadyExist = false; // Determines whther or not the given key-value pair already is in the table.
+            V value = pair.Value;
+            List<KeyValuePair<K, V>> bucket = table[hash];
+            
+            foreach(KeyValuePair<K,V> entry in bucket)
+            {
+                // Ensures no duplicates are created.
+                // Enters new value to existing key if the provided key-value key already exists.
+                if(entry.Key.Equals(pair.Key))
+                {
+                    value = entry.Value;
+                    bucket.Remove(entry);
+                    bucket.Add(new KeyValuePair<K, V>(pair.Key, pair.Value));
+                    alreadyExist = true;
+                    break;
+                }
+            }
+
+            if(!alreadyExist)
+            {
+                bucket.Add(pair);
+                numEntries++;
+            }
+
+           
+            int maxSize = (int)(loadFactor * table.Length);
+
+            // If the number of entires is greater than the threshold, we increase the hash map's capacity.
+            if (numEntries >= maxSize)
+            {
+                increaseCapacity();
+            }
+            
+            return value;
+        }
+
+        public override V remove(K key)
+        {
+            V removedValue = default(V);
+            int hash = hashCode(key);
+            List<KeyValuePair<K, V>> bucket = table[hash];
+
+            foreach(KeyValuePair<K,V> entry in bucket)
+            {
+                if(entry.Key.Equals(key))
+                {
+                    removedValue = entry.Value;
+                    bucket.Remove(entry);
+                    numEntries--;
+
+                    int hereLoadFactored = (int)(numEntries / loadFactor);
+                    int smallerSize = getSmallerSize(table.Length);
+
+                    if((hereLoadFactored < smallerSize) && (smallerSize > minimumSize))
+                    {
+                        reduceCapacity();
+                    }
+                }
+            }
+            
+            return removedValue;
+        }
+
+        /// <returns> The input multiplied by ten. </returns>
+        private int getLargerSize(int input)
+        {
+            return input * 10;
+        }
+
+        /// <returns> A quarter of the input. </returns>
+        private int getSmallerSize(int input)
+        {
+            return input / 4;
+        }
+
+        private void increaseCapacity()
+        {
+            List<KeyValuePair<K, V>>[] temp = table;
+
+            // Calculate the new size and assign it.
+            int size = getLargerSize(table.Length);
+            initialiseMap(size);
+
+            // Re-hash old data.
+            foreach(List<KeyValuePair<K,V>> list in temp)
+            {
+                foreach(KeyValuePair<K,V> entry in list)
+                {
+                    put(entry);
+                }
+            }
+
+        }
+        
+        private void reduceCapacity()
+        {
+            List<KeyValuePair<K, V>>[] temp = table;
+
+            // Calculate the new size and check minimum.
+            int length = getSmallerSize(table.Length);
+            initialiseMap(length);
+
+            foreach(List<KeyValuePair<K,V>> list in temp)
+            {
+                foreach(KeyValuePair<K,V> entry in list)
+                {
+                    put(entry);
+                }
+            }
+        }
+        
+        public override void clear()
+        {
+            for(int k = 0; k < table.Length; k++)
+            {
+                table[k].Clear();
+                numEntries--;
+            }
+        }
+    }
+}
