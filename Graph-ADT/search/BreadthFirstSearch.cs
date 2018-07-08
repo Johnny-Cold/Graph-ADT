ca58865539a18e4fbc8f28bac9c532ca7342b42b
Johnny-Cold <obakengmokote@gmail.com>
2018-07-08 19:16:38 +0200

    I decided to not implement the graph to test search algorithms used in AI (it's better to do so using an agent with the graph to fully define the program).
    Added Breadth-First Search algorithm.

---
+++ Graph-ADT/search/BreadthFirstSearch.cs
@@ -0,0 +1,54 @@
+﻿using Graph_ADT.graph;
+using Graph_ADT.map;
+using Graph_ADT.mod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_ADT.search
+{
+    public class BreadthFirstSearch<T>
+    {
+        public static void search(Graph<Vertex<T>> graph, Vertex<T> node, HashSet<Vertex<T>> known, HashMap<Vertex<T>, Edge<Vertex<T>>> forest)
+        {
+            LinkedList<Vertex<T>> level = new LinkedList<Vertex<T>>(); // List of vertices in the current graph level.
+            known.Add(node); // We add the start point to the set of known nodes.
+            level.AddLast(node); // The first level includes only the node "node".
+
+            while(level.Count > 0)
+            {
+                LinkedList<Vertex<T>> nextLevel = new LinkedList<Vertex<T>>(); // List of vertices in the following graph level.
+
+                foreach(Vertex<T> vertex in level)
+                {
+                    foreach(Edge<Vertex<T>> edge in graph.getEdges(vertex))
+                    {
+                        Vertex<T>[] endpoints = edge.getEndpoints();
+
+                        // Since breadth-first search expands OUTWARDLY from the current node, we only want to deal with outgoing edges. 
+                        if(!endpoints[0].Equals(vertex))
+                        {
+                            break;
+                        }
+
+                        Vertex<T> v = endpoints[1];
+
+                        if (v != null)
+                        {
+                            if (!known.Contains(v))
+                            {
+                                known.Add(v);
+                                forest.put(v, edge);
+                                nextLevel.AddLast(v);
+                            }
+                        }
+                    }
+
+                    level = nextLevel; // Proceeding to next level in graph.
+                }
+            }
+        }
+    }
+}
--- Graph-ADT/search/DepthFirstSearch.cs
+++ Graph-ADT/search/DepthFirstSearch.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Graph_ADT.search.uninformed
+namespace Graph_ADT.search
 {
     public class DepthFirstSearch<T>
     {
